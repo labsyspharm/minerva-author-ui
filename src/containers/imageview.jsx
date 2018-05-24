9b697f5cade398c21598736c35c613695e48cb94
John Hoffer <john@hoff.in>
2018-05-24 18:41:48 -0400

    plug in openseadragon... unsure why not rendering

--- src/containers/imageview.jsx
+++ src/containers/imageview.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 
 import viaWebGL from 'viawebgl';
-import AmazonWebSource from '../amazonwebsource'
+import AmazonWebSource from '../amazonwebsource';
+
+import '../style/imageview';
 
 class ImageView extends Component {
 
@@ -15,34 +17,54 @@ class ImageView extends Component {
         tileMargin: -1024,
         compositeOperation: "lighter",
         prefixUrl: "images/openseadragon/"
-      }
+      },
+			auth: {
+				AccessKeyId: process.env.ACCESSKEYID,
+				SessionToken: process.env.SESSIONTOKEN,
+			  SecretAccessKey: process.env.SECRETACCESSKEY
+			}
     }
-		console.log(process.env);
   }
 
   makeTileSource(entry) {
+    const {auth} = this.state;
+    const {url} = this.props.imageOptions;
+
     const [id, channelcontrol] = entry;
-    const {imageOptions} = this.props;
     const {color, range} = channelcontrol;
 
+		const getTileName = (x, y, level, channel) => {
+			return "C" + channel + "-T0-Z0-L" + level + "-Y" + y + "-X" + x + ".png";
+		}
+
+		const getTileUrl = function(l, x, y) {
+			var level = this.maxLevel - l;
+			var url = this.many_channel_url;
+			var channel = this.many_channel_id;
+
+			var name = getTileName(x, y, level, channel);
+			return url + '/' + name;
+		}
+
     var output = {
+			// Custom functions
+    	makeAjaxRequest: new AmazonWebSource(auth).makeAjaxRequest,
+			getTileUrl: getTileUrl,
+			// CUstom parameters
       many_channel_id: id,
+			many_channel_url: url,
       many_channel_range: range,
-      many_channel_color: map(color, c => c / 255.)
+      many_channel_color: map(color, c => c / 255.),
+			// Standard parameters
+			tileSize: 1024,
+			height: 4080,
+			width: 7220,
+			minLevel: 0,
+			maxLevel: 3
     }
 
-    // Many channels in tiled image
-    output.getTileUrl = getTileUrl;
-    output.many_channel_url = url;
-    output.tileSize = 1024;
-    output.height = 4080;
-    output.width = 7220;
-    output.minLevel = 0;
-    output.maxLevel = 3;
-
     // Make AWS request
-    var aws = new AmazonWebSource(credentials);
-    output.makeAjaxRequest = aws.makeAjaxRequest; 
+    output.makeAjaxRequest = aws; 
 
     return output
   }
@@ -60,9 +82,9 @@ class ImageView extends Component {
 
     // Set up openseadragon viewer
     var viewer = viaWebGL.OpenSeadragon({
+      id: "ImageView",
       debugMode: false,
       collectionMode: true,
-      id: "many-channel-viewer",
       showZoomControl: false,
       showHomeControl: false,
       showFullPageControl: false,
@@ -78,6 +100,48 @@ class ImageView extends Component {
       tileSources: tileSources
     });
 
+    // Define interface to shaders
+    var seaGL = new viaWebGL.openSeadragonGL(viewer);
+    seaGL.vShader = 'static/vert.glsl';
+    seaGL.fShader = 'static/frag.glsl';
+
+    seaGL.addHandler('tile-drawing',  function(callback, e) {
+			// Read parameters from each tile 
+			var tile = e.tile;
+			var via = this.viaGL;
+			var viewer = this.openSD;
+			var image = e.tiledImage;
+			var source = image.source;
+
+			// Store channel color and range to send to shader
+			via.color_3fv = new Float32Array(source.many_channel_color);
+			via.range_2fv = new Float32Array(source.many_channel_range);
+	 
+			// Start webGL rendering
+			callback(e);
+  	});
+
+    seaGL.addHandler('gl-drawing', function() {
+			// Send color and range to shader
+			this.gl.uniform3fv(this.u_tile_color, this.color_3fv);
+			this.gl.uniform2fv(this.u_tile_range, this.range_2fv);
+
+			// Clear before each draw call
+			this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+  	});
+
+    seaGL.addHandler('gl-loaded', function(program) {
+			// Turn on additive blending
+			this.gl.enable(this.gl.BLEND);
+			this.gl.blendEquation(this.gl.FUNC_ADD);
+			this.gl.blendFunc(this.gl.ONE, this.gl.ONE);
+
+			// Uniform variable for coloring
+			this.u_tile_color = this.gl.getUniformLocation(program, 'u_tile_color');
+			this.u_tile_range = this.gl.getUniformLocation(program, 'u_tile_range');
+		});
+
+    seaGL.init();
   }
 
   render() {
@@ -85,7 +149,7 @@ class ImageView extends Component {
     const entries = channelMap.entries();
 
     return (
-      <div id="many-channel-viewer"></div>
+      <div id="ImageView"></div>
     );
   }
 }
