ff7d59988b3506cd655d7d5c8d20340248c52562
thejohnhoffer <hoffexi@gmail.com>
2018-05-27 20:26:47 -0400

    update lockfile, script, and readme

---
+++ README.md
@@ -0,0 +1,22 @@
+### Getting a token
+
+Install and configure the AWS CLI
+
+```
+pip install awscli
+aws configure
+```
+
+Run this to write temporary credentials to `.env`
+
+```
+python dotenv.py
+```
+
+### Building the demo
+
+```
+npm i && npm run start
+```
+
+The server will be running on port `8487`.
--- dotenv.py
+++ dotenv.py
@@ -6,13 +6,20 @@ import json
 def make_credentials():
     ''' parse results of aws command '''
     response = os.popen('aws sts get-session-token').read()
-    credentials = json.loads(response)['Credentials']
 
-    for k in ['SessionToken', 'AccessKeyId', 'SecretAccessKey']:
-        yield "{0}={1}".format(k.upper(), credentials[k])
+    # Try to read credentials from response
+    try:
+        credentials = json.loads(response)['Credentials']
+
+        for k in ['SessionToken', 'AccessKeyId', 'SecretAccessKey']:
+            yield "{0}={1}".format(k.upper(), credentials[k])
+
+    except json.decoder.JSONDecodeError as e:
+        print(e)
 
 content = list(make_credentials())
 
-with open('.env', 'w') as env:
-    for line in content:
-        env.write(line+'\n')
+if content:
+    with open('.env', 'w') as env:
+        for line in content:
+            env.write(line+'\n')
