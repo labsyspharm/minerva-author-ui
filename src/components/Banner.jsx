1de3c53cb319cd7efd89b85dde8c495a8c7ec2d5
thejohnhoffer <hoffexi@gmail.com>
2018-05-29 17:07:28 -0400

    revert to correct navbar alignment

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -12,14 +12,13 @@ const Banner = ({ session, handleLogin, handleLogout }) => {
   return (
   <nav className="navbar navbar-expand-lg navbar-dark">
     <a className="navbar-brand" href="#">Minerva WebGL2 Viewer</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
     <div id="navbarNavDropdown" className="navbar-collapse collapse">
       <ul className="navbar-nav mr-auto">
         <li className="nav-item active">
           <a className="nav-link" href="#">Repositories<span className="sr-only">(current)</span></a>
         </li>
+       </ul>
+       <ul className="navbar-nav">
         <li className="nav-item">
 					<Dropdown>
 					</Dropdown>
