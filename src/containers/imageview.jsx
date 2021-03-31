e62d1996d0e0013d39e6f2b92d6265280bec1533
John Hoffer <john@hoff.in>
2021-03-30 22:06:27 -0400

    rename fragment shader to aid in cachebusting

--- src/containers/imageview.jsx
+++ src/containers/imageview.jsx
@@ -324,7 +324,7 @@ class ImageView extends Component {
           return this.gl.canvas;
       }
       seaGL.vShader = 'vert.glsl';
-      seaGL.fShader = 'frag.glsl?cache=v1.7';
+      seaGL.fShader = 'frag_v1_7_1.glsl';
 
       seaGL.addHandler('tile-drawing',  function(callback, e) {
 
